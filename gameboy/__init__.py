398054aee8abc0d5aef7c43b996415ade2206a93
Mickaël Schoentgen <contact@tiger-222.fr>
2018-05-08 16:56:05 +0200

    Add basic files

---
+++ gameboy/__init__.py
@@ -0,0 +1,22 @@
+"""
+Game Boy emulator written from scratch in Python 3. Project code name: Sarita.
+
+This project is authored by Mickaël Schoentgen <contact@tiger-222.fr>.
+
+You can always get the latest version of this module at:
+    https://github.com/BoboTiG/PyGameBoy
+If that URL should fail, try contacting the author.
+"""
+__version__ = '0.0.1-dev'
+__author__ = "Mickaël 'Tiger-222' Schoentgen"
+__copyright__ = """
+Copyright (c) 2018, Mickaël 'Tiger-222' Schoentgen
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose and without fee or royalty is hereby
+granted, provided that the above copyright notice appear in all copies
+and that both that copyright notice and this permission notice appear
+in supporting documentation or portions thereof, including
+modifications, that you make.
+"""
+__all__ = ()
